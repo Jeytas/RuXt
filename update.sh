34c56472d2746a3e204665618dbbff5bd97801dc
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-07 18:42:54 +0100

    update.sh updated

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
-rm -rf ~/.ruxt
+#!/bin/bash
+echo "Removing old directory"
+rm -r ~/.ruxt
+if [ $? = 0 ]; then echo "Removing successful. Now cloning repository"; else echo "Removing unsuccessful, update aborted" && exit; fi
+
 git clone https://github.com/Jeytas/RuXt.git ~/.ruxt
-echo "Old directory removed and replaced with new one"
+if [ $? = 0 ]; then echo "Cloning successful, program has been updated"; else echo "Cloning unsucessful, update aborted" && exit; fi
